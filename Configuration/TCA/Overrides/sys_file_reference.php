1f660e1782edbc29560f13267e065b779cdce83d
Alfred Bez <alfred.bez@googlemail.com>
2015-07-24 12:30:09 +0200

    add default configuration as comment

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -3,6 +3,19 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+/*
+    Defaults
+
+    'ratios' => array(
+        '1.7777777777777777' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.16_9',
+        '1.3333333333333333' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.4_3',
+        '1' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.1_1',
+        'NaN' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.free',
+    ),
+
+*/
+
 $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config'] = array(
     'type' => 'imageManipulation',
     'ratios' => array(
